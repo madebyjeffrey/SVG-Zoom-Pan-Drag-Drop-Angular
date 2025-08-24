752a45efd5cfc0185513ccde5677ae886109d482
Jeffrey Drake <jeff@ideaplex.ca>
2025-08-24 17:24:53 -0600

    angular 19 upgrade

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,10 @@ enum DragOperationTypes {
 
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: false
 })
 export class AppComponent implements AfterViewInit{
   
